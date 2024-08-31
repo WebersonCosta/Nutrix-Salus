d915389d42cc1db183853549d1d2cf1bf5479b35
Weberson Costa <weberson.dev0@gmail.com>
2024-08-31 16:10:11 -0300

    Back-to-top-button-e-ajustes

--- assets/js/js-scripts.js
+++ assets/js/js-scripts.js
@@ -51,6 +51,41 @@ const mobileNavbar = new MobileNavbar(
 
 mobileNavbar.init();
 
+// Código ScrollReveal para animação suave ao rolar a página
+const sr = ScrollReveal({
+    origin: 'bottom',   // direção da animação
+    distance: '50px',   // distância do movimento do elemento
+    duration: 1000,     // duração da animação
+    delay: 200,         // atraso antes da animação começar
+    reset: false        // se a animação deve repetir ao rolar de volta
+});
+
+// Aplicar a animação aos elementos com a classe .section
+sr.reveal(
+    `#about .text-about,
+    #products .container,
+    #products .content-section,
+    #articles .content-section,
+    #articles .card
+    footer
+    `,{interval: 100})
+
+// ================== BACK TO TOP BUTTON ===================== //
+
+const backToTopButton = document.querySelector('.back-to-top')
+function backToTop(){
+    
+    if(window.scrollY >= 560){
+        backToTopButton.classList.add('show')
+    }else{
+        backToTopButton.classList.remove('show')
+    }
+}
+
+window.addEventListener('scroll', function(){
+    backToTop()
+})
+
 /*class MobileNavbar{
     constructor(mobileMenu, navList, navLinks){
         this.mobileMenu = document.querySelector(mobileMenu);
